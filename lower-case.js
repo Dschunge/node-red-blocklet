a231921ef0b2d34f6ac4eb78288a9b68e6d61e30
Juergen Schaar <juergenschaar@Juergens-MBP.fritz.box>
2020-02-29 16:43:05 +0100

    modified3

--- lower-case.js
+++ lower-case.js
@@ -1,6 +1,8 @@
 module.exports = function(RED) {
     function LowerCaseNode(config) {
         RED.nodes.createNode(this,config);
+        console.log('config: ', config);
+        this.address = config.address;
         var node = this;
         node.on('input', function(msg) {
             msg.payload = msg.payload.toLowerCase();
