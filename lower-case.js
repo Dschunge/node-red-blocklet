5df1f881be1499cc92459e500f0dbfde54d337f5
Juergen Schaar <juergenschaar@Juergens-MBP.fritz.box>
2020-03-01 13:59:33 +0100

    modified32

--- lower-case.js
+++ lower-case.js
@@ -1,11 +1,11 @@
+const WebSocket = require('ws'); 
+
 module.exports = function(RED) {    
     function LowerCaseNode(config) {
         var node = this;
         connect = (socket, address) => {
-            const BLOCK = 0, UNCLE = 1, TXN = 2, INTERNAL_MSG = 3
-            const cmd = `{"jsonrpc":"2.0","id":"${TXN}","method":"subscribe","params":["address:transactions",{"address":"${address}"}]}`
-            console.log('jsonrpc: ', cmd)
-            socket.send(cmd);           
+            const BLOCK = 0, UNCLE = 1, TXN = 2, INTERNAL_MSG = 3;            
+            socket.send(`{"jsonrpc":"2.0","id":"${TXN}","method":"subscribe","params":["address:transactions",{"address":"${address}"}]}`);           
         }   
 
         RED.nodes.createNode(this, config);        
@@ -13,21 +13,14 @@ module.exports = function(RED) {
         if (config && config.address && config.apikey && config.network) {
             this.address = config.address;
             this.apikey = config.apikey;
-            this.network = config.network;
-            const WebSocket = require('ws');
-            //const socket = new WebSocket(`wss://ws.web3api.io?x-api-key=${this.apikey}&x-amberdata-blockchain-id=ethereum-rinkeby`);
+            this.network = config.network;                       
             const socket = new WebSocket(`wss://ws.web3api.io?x-api-key=${this.apikey}&x-amberdata-blockchain-id=${this.network}`);
 
             // Connection opened
             socket.addEventListener('open', (event) => {
                 console.log('Connection opened - ');
-                connect(socket, this.address)
-                //this.status({fill:"red",shape:"dot",text:"Error: Choose an action"});
-                node.status({
-                    fill:"green",
-                    shape:"dot",
-                    text:"Connected..."
-                });
+                connect(socket, this.address)                
+                node.status({ fill:"green", shape:"dot", text:"Connected..."});
             });
 
             // Listen for messages
@@ -40,8 +33,7 @@ module.exports = function(RED) {
                     node.send(msg);
                 }
             });
-
-            // Listen for msgs
+            // Connection closed
             socket.addEventListener('close', (event) => {
                 console.log('Connection closed - ', event);
                 node.status({fill:"red",shape:"dot",text:"Error: Connection closed."});
